d65e9e8c226d76c3cfe24f198b8e62f442d9dd76
eddyjc <56493855+eddyjc@users.noreply.github.com>
2019-10-13 11:37:53 +1100

    Login Page (Skeleton) Created

---
+++ Login/app.html
@@ -0,0 +1,19 @@
+<?
+session_start();
+?>
+
+<!DOCTYPE html>
+<html> 
+<body>
+<h1>WELCOME 
+
+<? 
+	$employeename = $_SESSION["Employee"];
+	echo "$employeename"; 
+
+?>
+
+
+</h1>
+</body>
+</html>
---
+++ Login/login.php
@@ -0,0 +1,41 @@
+<?php 
+session_start();
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "phpdb";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if($conn->connect_error) {
+	die("Connection Failed: " . $conn->connect_error);
+}
+
+if (isSet($_POST['usern']) && isSet($_POST['pass'])) {
+	$user=$_POST['usern'];
+	$pass=$_POST['pass'];
+	
+$query = "SELECT * FROM login WHERE username='$user' and Password='$pass'";
+$result = mysqli_query($conn, $query);
+$count = mysqli_num_rows($result);
+$row = mysqli_fetch_array($result);
+
+$employeename= $row['FirstName'] . " " . $row['LastName'];
+#$_SESSION["Employee"] = $employeename;
+
+#$test = $_SESSION["Employee"];
+
+if ($count == 1) {
+	$_SESSION["Employee"] = $employeename;
+	header('location:app.php');
+} else {
+	header('location: index.html');
+}
+#echo "$test";
+} else {
+	header('location: index.html');
+}
+
+
+mysqli_close($conn);
+?>
